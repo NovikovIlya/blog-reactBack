660f90f2ce01050ccce9c622cbfe10d129c38c7e
NovikovIlya <novikov.i.sergeevich@gmail.com>
2023-08-21 22:05:43 +0300

    11

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 import mongoose from 'mongoose';
 import multer from 'multer';
 import {
@@ -24,6 +25,11 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')){
+      fs.mkdirSync('uploads')
+    }
+    
+    
     cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
